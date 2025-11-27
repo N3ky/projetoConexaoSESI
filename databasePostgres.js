50dee2030c1a1a16330b2868a89f6475ecce8afc
Nicole Rodriguez Colmenares <127136695+N3ky@users.noreply.github.com>
2025-11-27 16:22:48 -0300

    Update databasePostgres.js

--- databasePostgres.js
+++ databasePostgres.js
@@ -1,39 +1,53 @@
 import { randomUUID } from "node:crypto";
 import { sql } from "./sql.js";
 
-export class DatabasePostgres{
-        async list(search){
-            let result;
 
-            if(search){
-                result = await sql`SELECT * FROM videos WHERE title ILIKE ${'%' + search + '%'}`;
-            }
-            else{
-                result = await sql`SELECT * FROM videos`;
-            }
-            //Garante que sempre retorna um array
-            return Array.isArray(result)? result :  result.rows || []
-        }
-        async create(video){
-            const video = randomUUID();
-            const {title, description, duration} = video;
+export class DatabasePostegres {
+  async list(search) {
+    let result;
 
-            await sql`
-                    INSERT INTO videos(id, title, description,duration)
-                    VALUES (${videoId}, ${title}, ${description}, ${duration})
-            `;
-        }
 
-        async update(id,video){
-            const{title, description, duration} = video;
+    if (search) {
+      result = await sql`SELECT * FROM videos WHERE title ILIKE ${'%' + search + '%'}`;
+    } else {
+      result = await sql`SELECT * FROM videos`;
+    }
 
-            await sql`
-                UPDATE videos
-                SET title= ${title},description= ${description}, duration= ${duration}
-                WHERE id= ${id}
-            `
-        }
-        async delete(id){
-            await sql`DELETE FROM videos WHERE id= ${id}`;
-        }
+
+    // garante que sempre retorna um array
+    return Array.isArray(result) ? result : result.rows || [];
+  }
+
+
+  async create(video) {
+    const videoId = randomUUID();
+    const { title, description, duration } = video;
+
+
+    await sql`
+      INSERT INTO videos(id, title, description, duration)
+      VALUES (${videoId}, ${title}, ${description}, ${duration})
+    `;
+  }
+
+
+  async update(id, video) {
+    const { title, description, duration } = video;
+
+
+    await sql`
+      UPDATE videos
+      SET title = ${title}, description = ${description}, duration = ${duration}
+      WHERE id = ${id}
+    `;
+  }
+
+
+  async delete(id) {
+    await sql`DELETE FROM videos WHERE id = ${id}`;
+  }
 }
+
+
+
+
