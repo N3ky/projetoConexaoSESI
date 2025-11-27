bb25643b50730b0467af13d08d1ba62fd21870b7
Nicole Rodriguez Colmenares <127136695+N3ky@users.noreply.github.com>
2025-11-27 16:21:33 -0300

    Update createTable.js

--- createTable.js
+++ createTable.js
@@ -1,24 +1,21 @@
-import { sql } from './sql.js'
+import { sql } from './sql.js';
 
 async function criarTabela() {
-    try{
-        await sql`
-            CREATE TABLE IF NOT EXIST videos(
-                id  UUID PRIMARY KEY,
-                title TEXT NO NULL,
-                description TEXT NO NULL,
-                duration INTEGER NO NULL
-            );
-        `;
-        console.log("Tabela 'videos' criada com sucesso!");
-    }
-
-    catch(err){
-        console.log("Erro ao criar a tabela 'videos':",err);
-    }
-    finally{
-        process.exit();
-    }
-    
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS videos (
+        id UUID PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        duration INTEGER NOT NULL
+      );
+    `;
+    console.log("Tabela 'videos' criada com sucesso!");
+  } catch (err) {
+    console.error("Erro ao criar a tabela:", err);
+  } finally {
+    process.exit();
+  }
 }
+
 criarTabela();
