37dbe6d5c81ab05a50751dd48995b67a3c927691
Nicole Rodriguez Colmenares <127136695+N3ky@users.noreply.github.com>
2025-11-27 16:24:14 -0300

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,5 +1,5 @@
-import { neon } from "@neondatabase/serverless"
-import 'dotenv/config'
+import { neon } from "@neondatabase/serverless";
+import 'dotenv/config';
 
 //a URL do banco vem do arquivo .env
-export const sql = neon(process.env.DATABASE_URL)
+export const sql = neon(process.env.DATABASE_URL);
